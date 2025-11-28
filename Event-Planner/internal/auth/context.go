9ca4f7c10d95af5c02c13cb8f0d3940754db45c7
dohasheriff <150528688+dohasheriff@users.noreply.github.com>
2025-11-28 22:32:16 +0200

    Update context.go

--- Event-Planner/internal/auth/context.go
+++ Event-Planner/internal/auth/context.go
@@ -6,13 +6,14 @@ type contextKey string
 
 const userIDKey contextKey = "user_id"
 
-// setUserID adds user ID to context
+//add user ID to context
 func setUserID(ctx context.Context, userID int) context.Context {
 	return context.WithValue(ctx, userIDKey, userID)
 }
 
-// GetUserID retrieves user ID from context
+//retrieve user ID from context
 func GetUserID(ctx context.Context) (int, bool) {
 	userID, ok := ctx.Value(userIDKey).(int)
 	return userID, ok
 }
+
